3b1d8c1ef52fa4bfe1aafed963d152118711d0dd
oszegda <oszegda@icloud.com>
2023-05-06 22:26:41 +0200

    Added before action for counting requests in session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :count_requests_in_session
+
+  def count_requests_in_session
+    session[:requests] ||= 0
+    session[:requests] += 1
+  end
 end
